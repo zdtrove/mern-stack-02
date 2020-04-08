9be31d4d773d2494f9400831a012a19a67df13f7
Hoang An <antnh@icd-vn.com>
2020-04-08 16:41:49 +0700

    cm

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://antnh:721904791992@cluster0-qpbfw.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const items = require('./routes/api/items');
+
+const app = express();
+app.use(express.json());
+
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.error(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
